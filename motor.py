c6b2f65d75c8fb62776c0e415b9a4c7ce040b571
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-08-02 16:12:39 -0400

    Update motor.py

--- motor.py
+++ motor.py
@@ -6,20 +6,20 @@ gpio.setup(17, gpio.OUT)
 gpio.setup(22, gpio.OUT)
 gpio.setup(27, gpio.OUT)
 
-gpio.output(17, false)
+gpio.output(17, False)
 
 time.sleep(4)
 
-gpio.output(22, true)
-gpio.output(27, false)
+gpio.output(22, True)
+gpio.output(27, False)
 
-gpio.output(17, true)
-
-time.sleep(4)
-
-gpio.output(27, true)
-gpio.output(22, false)
+gpio.output(17, True)
 
 time.sleep(4)
 
-gpio.output(17, false)
+gpio.output(27, True)
+gpio.output(22, False)
+
+time.sleep(4)
+
+gpio.output(17, False)
