17b0b12dbdd5b7ea6402bf88b621c9b50fe85595
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-08-02 16:11:52 -0400

    Update motor.py

--- motor.py
+++ motor.py
@@ -2,9 +2,9 @@ import RPi.GPIO as gpio
 import time
 
 gpio.setmode(gpio.BCM)
-gpio.setup(17, gpio.out)
-gpio.setup(22, gpio.out)
-gpio.setup(27, gpio.out)
+gpio.setup(17, gpio.OUT)
+gpio.setup(22, gpio.OUT)
+gpio.setup(27, gpio.OUT)
 
 gpio.output(17, false)
 
