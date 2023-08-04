bdbd8ee9268a93dea942187195cb577580ea48e7
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-08-04 17:36:02 -0400

    cxz

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const io = require('socket.io-client')
 const socket = io.connect('http://192.168.2.14:3000', { reconnect: true });
+// const socket = io.connect('http://192.168.2.18:3000', { reconnect: true });
 
 socket.on('connect', function () {
     console.log("socket connected"); 
