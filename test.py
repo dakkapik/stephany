fce877ac1b65e1720559b09ff32d972f580200c7
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-08-04 14:45:43 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,19 @@
 import socketio
 
-print (socketio.Client)
-# sio = socketio.Client()
+sio = socketio.Client()
 
-# @sio.event
-# def connect():
-#     print('connection established')
+@sio.event
+def connect():
+    print('connection established')
 
-# @sio.event
-# def my_message(data):
-#     print('message received with ', data)
-#     sio.emit('my response', {'response': 'my response'})
+@sio.event
+def my_message(data):
+    print('message received with ', data)
+    sio.emit('my response', {'response': 'my response'})
 
-# @sio.event
-# def disconnect():
-#     print('disconnected from server')
+@sio.event
+def disconnect():
+    print('disconnected from server')
 
-# sio.connect('http://192.168.2.14:3000')
-# sio.wait()
+sio.connect('http://192.168.2.14:3000')
+sio.wait()
