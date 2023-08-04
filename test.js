6dd7df0b1937aad5ab93a44e7c4e69fe5dffe4d2
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-08-04 16:59:54 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-const { io } = require("socket.io-client");
-const socket = io("http://192.168.2.14:3000")
+const io = require('socket.io-client')
+const socket = io.connect('http://192.168.2.14:3000', { reconnect: true });
 
-socket.on("connect", ()=> {
-    console.log("CONNECTED")
-})
+socket.on('connect', function () {
+    console.log("socket connected"); 
+});
+
+socket.on('broadcast', function (data) {
+    console.log("we got a broadcast: ", data);
+});
